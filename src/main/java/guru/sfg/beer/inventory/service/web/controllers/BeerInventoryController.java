76845e4efcdea25da09536c092f039643b915ab6
helen <hhma@alumni.cpp.edu>
2020-08-10 17:43:02 -0400

    add a post method for my own test

--- src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
@@ -1,13 +1,12 @@
 package guru.sfg.beer.inventory.service.web.controllers;
 
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
 import guru.sfg.beer.inventory.service.web.mappers.BeerInventoryMapper;
 import guru.sfg.brewery.model.BeerInventoryDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -32,5 +31,11 @@ public class BeerInventoryController {
                 .stream()
                 .map(beerInventoryMapper::beerInventoryToBeerInventoryDto)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping("api/v1/beer/add")
+    public void postBeer(@RequestBody BeerInventoryDto beerInventoryDto){
+        BeerInventory bi =  beerInventoryMapper.beerInventoryDtoToBeerInventory(beerInventoryDto);
+        beerInventoryRepository.save(bi);
     }
 }
